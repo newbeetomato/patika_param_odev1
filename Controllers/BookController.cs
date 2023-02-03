9898107959a6a84a5f92a0b3029ab6e47e38cba8
ibrahimnureddin <ibrahimnureddin4job@gmail.com>
2023-02-03 04:38:29 +0300

    500 eror code added

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -36,27 +36,31 @@ namespace WebApi.AddControllers
         [HttpGet]
         public List<Book> GetBooks([FromQuery] string? ShortType) //Get işlemi
         {
-            List<Book> bookList;
-            bookList = BookList.ToList<Book>();
-            if (ShortType is not null)
-            {
 
-                if (ShortType == "a-z")//A dan Z ye listeleme işlemi
-                {
-                    bookList = BookList.OrderBy(x => x.Title).ToList<Book>();
-                    return bookList;
-
-                }
-                if (ShortType == "z-a")//Z den A ye listeleme işlemi
+            
+                List<Book> bookList;
+                bookList = BookList.ToList<Book>();
+                if (ShortType is not null)
                 {
 
-                    bookList = BookList.OrderByDescending(x => x.Title).ToList<Book>();
-                    return bookList;
+                    if (ShortType == "a-z")//A dan Z ye listeleme işlemi
+                    {
+                        bookList = BookList.OrderBy(x => x.Title).ToList<Book>();
+                        return bookList;
+
+                    }
+                    if (ShortType == "z-a")//Z den A ye listeleme işlemi
+                    {
+
+                        bookList = BookList.OrderByDescending(x => x.Title).ToList<Book>();
+                        return bookList;
+                    }
+
+
                 }
-
-
-            }
-            return bookList;
+                return bookList;
+           
+            
 
 
 
@@ -65,51 +69,86 @@ namespace WebApi.AddControllers
         [HttpGet("{id}")]
         public Book GetById([FromRoute] int id) //from route id ile get işlemi
         {
-            var book = BookList.Where(book => book.Id == id).SingleOrDefault();//highorder
-            if (book == null)
-            {
-                return BookList.First();
-            }
-            return book;
+
+            
+                var book = BookList.Where(book => book.Id == id).SingleOrDefault();//highorder
+                if (book == null)
+                {
+                    return BookList.First();
+                }
+                return book;
+           
+           
+            
         }
 
         [HttpPost]
         public IActionResult AddBook([FromBody] Book newBook) 
         {
-            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
-            if (book != null)
-                return BadRequest();
 
-            BookList.Add(newBook);
+            try
+            {
+                var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+                if (book != null)
+                    return BadRequest();
 
-            return Created("201", newBook); // 201 kodu
+                BookList.Add(newBook);
+
+                return Created("201", newBook); // 201 kodu
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            }
+            
         }
 
         [HttpPut("{id}")]
         public IActionResult UpdateBook( int id, [FromBody] Book updatedBook) //İd ye göre değişitirlen kısımları Update 
         {
-            var book = BookList.SingleOrDefault(x => x.Id == id);
-            if (book == null)
-                return NotFound(); // 404 kodu
+            try
+            {
+                var book = BookList.SingleOrDefault(x => x.Id == id);
+                if (book == null)
+                    return NotFound(); // 404 kodu
 
-            book.GenereId = updatedBook.GenereId != default ? updatedBook.GenereId : book.GenereId;
-            // verisi varsa genreid yi update et yoksa kendi değerini kullan
-            book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
-            book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
-            book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+                book.GenereId = updatedBook.GenereId != default ? updatedBook.GenereId : book.GenereId;
+                // verisi varsa genreid yi update et yoksa kendi değerini kullan
+                book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+                book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
+                book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
 
-            return Ok(); //200
+                return Ok(); //200
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            }
+            
 
         }
 
         [HttpPatch("{id}")]
         public IActionResult UpdateBookPatch( int id, [FromBody] JsonPatchDocument<Book> updatedBookPatch)
         {
-            var book=BookList.SingleOrDefault(x=>x.Id==id);
-            if(book==null)
-             {return NotFound(); }
-            updatedBookPatch.ApplyTo(book,ModelState);
-            return Ok(book);
+            try
+            {
+                var book = BookList.SingleOrDefault(x => x.Id == id);
+                if (book == null)
+                { return NotFound(); }
+                updatedBookPatch.ApplyTo(book, ModelState);
+                return Ok(book);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            
 
 
         }
@@ -118,12 +157,22 @@ namespace WebApi.AddControllers
         [HttpDelete("{id}")]
         public IActionResult DeleteBook(int id)
         {
-            var book = BookList.SingleOrDefault(x => x.Id == id);
-            if (book == null)
-                return BadRequest(); // 400 Http durum kodu
+            try
+            {
+                var book = BookList.SingleOrDefault(x => x.Id == id);
+                if (book == null)
+                    return BadRequest(); // 400 Http durum kodu
 
-            BookList.Remove(book);
-            return Ok(); //200 durum kodu
+                BookList.Remove(book);
+                return Ok(); //200 durum kodu
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            }
+
         }
     }
 }
